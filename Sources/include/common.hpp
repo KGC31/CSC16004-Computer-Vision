90619c9236b4d3fb9539a1ea50c7d4682c02dce0
Nguyễn Việt Kim <kimnguyenblc@gmail.com>
2024-03-01 14:03:25 +0700

    Create common include file

---
+++ Sources/include/common.hpp
@@ -0,0 +1,16 @@
+// common.hpp
+/* This file include all the required libraries for all functions */
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <cmath>
+#include <iostream>
+#include <time.h>
+#include <opencv2/opencv.hpp>
+#include <map>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+#endif
